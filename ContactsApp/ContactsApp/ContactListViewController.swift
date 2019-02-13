c580d8314c75b4671cd639106e1dc9d87d9dac88
Radhika <radhika_ks@apple.com>
2019-02-13 14:53:02 +0530

    Initial setup, added contacts table view and controller file

---
+++ ContactsApp/ContactsApp/ContactListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ContactListViewController.swift
+//  ContactsApp
+//
+//  Created by Radhika KS01 on 2019-02-13.
+//  Copyright © 2019 Radhika. All rights reserved.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
+
+extension ContactListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        return cell
+    }
+}
---
+++ ContactsApp/ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Radhika KS01 on 2019-02-13.
+//  Copyright © 2019 Radhika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
