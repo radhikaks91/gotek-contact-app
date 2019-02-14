ecd0fbf4a334768eb6fc4c124ec04abf5a5ed1f5
Radhika <radhika_ks@apple.com>
2019-02-14 13:09:17 +0530

    Added Contact Detail and Edit Contact views

---
+++ ContactsApp/ContactsApp/ContactDetailsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ContactDetailsViewController.swift
+//  ContactsApp
+//
+//  Created by Radhika KS01 on 2019-02-14.
+//  Copyright © 2019 Radhika. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ContactDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "contactDetailCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
